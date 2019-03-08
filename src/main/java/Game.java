2d40e06b763c4660661c6f115d8b328425eaf006
Lars van Vliet <123321@student.drenthecollege.nl>
2019-03-08 11:20:37 +0100

    getWordsArray toegevoegd, geeft een array met 30 woorden. Closed #2

---
+++ src/main/java/Game.java
@@ -0,0 +1,13 @@
+package main.java;
+
+public class Game {
+
+    private String[] getWordsArray() {
+        return new String[]{
+                "appel", "banaan", "pelikaan", "auto", "laptop", "beeldscherm", "toetsenbord", "natuur", "lampen",
+                "jassen", "adapter", "lichtbron", "bankstel", "stekker", "oordopjes", "koptelefoon", "kruk", "bar",
+                "games", "muis", "plafonniere", "wieldopjesfabriek", "autodrop", "galgje", "internet", "desktop",
+                "monitor", "oerwoud", "dinosaurus", "programmeur"
+        };
+    }
+}
