1d5682c674c556bd520b0106a3287ff8284e5436
Sander Cokart <cokart32@gmail.com>
2019-03-11 08:51:50 +0100

    removed fixed size, Title set to "Galgje"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,8 +11,8 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 1000, 700));
+        primaryStage.setTitle("Galgje");
+        primaryStage.setScene(new Scene(root));
         primaryStage.setResizable(false);
         primaryStage.show();
     }
