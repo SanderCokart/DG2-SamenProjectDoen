fd4155a065b214e4eb59a4c59b038d1ba0b2fff8
Sander Cokart <cokart32@gmail.com>
2019-03-11 09:24:51 +0100

    methode getRandomWord aangemaakt die een random word returned. closed #1

--- src/main/java/Model.java
+++ src/main/java/Model.java
@@ -1,5 +1,7 @@
 package main.java;
 
+import java.util.Random;
+
 public class Model {
 
     private String[] getWordsArray() {
@@ -10,4 +12,16 @@ public class Model {
                 "monitor", "oerwoud", "dinosaurus", "programmeur"
         };
     }
+
+    public String getRandomWord() {
+        Random rand = new Random();
+
+        // Obtain a number between [0 - 30].
+        int randomNumber = rand.nextInt(30);
+
+        String[] words = getWordsArray();
+
+        return words[randomNumber];
+    }
+
 }
